9fdba3aaefdc466354f741a40fdfae915ee19f0d
David M. Chen <chen7david@me.com>
2022-11-14 18:12:07 +0800

    added select date box

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React + David!</h1>
+      <DatePicker style={{ marginRight: '8px' }} />
       <DatePicker />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
