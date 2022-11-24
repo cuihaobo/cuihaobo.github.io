a5a01023e6153671a659bbdd09da1f239f47934e
David M. Chen <chen7david@me.com>
2022-11-24 20:54:08 +0800

    updated name

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + David!</h1>
+      <h1>Vite + React + Lele!</h1>
       <DatePicker style={{ marginRight: '8px' }} />
       <DatePicker />
       <div className="card">
