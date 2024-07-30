851852c3ca70147c5165ff756725f39af2918ffb
David Chen <chen7david@me.com>
2024-07-30 09:03:14 -0400

    chore: update page to test cache

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex items-center justify-center h-screen w-screen bg-blue-200">
-      <div className="font-bold text-3xl text-white">Coming soon!</div>
+      <div className="font-bold text-3xl text-white">Coming later today!</div>
     </div>
   );
 }
